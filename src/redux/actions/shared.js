2594234cb7c7ccd446f457d510db62399da9a3a6
rabobahago <rabobahago@gmail.com>
2021-10-25 09:51:29 +0100

    feat:shared users and action creators add

---
+++ src/redux/actions/shared.js
@@ -0,0 +1,19 @@
+import { getInitialData } from "../../utils/api";
+import { receiveUsers } from "./users";
+import { receiveQuestions } from "./questions";
+import { setAuthedUser } from "./authedUser";
+
+export function handleInitialData() {
+  return (dispatch) => {
+    return getInitialData().then(({ users, questions }) => {
+      dispatch(receiveUsers(users));
+      dispatch(receiveQuestions(questions));
+    });
+  };
+}
+
+export function authenticate(authedId) {
+  return (dispatch) => {
+    dispatch(setAuthedUser(authedId));
+  };
+}
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,3 +1,3 @@
 import { combineReducers } from "redux";
-import users from "../reducers/userReducer";
+import users from "./users";
 export default combineReducers({ users });
