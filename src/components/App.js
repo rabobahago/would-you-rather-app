89ed55550b61e04cc127fcb46ae09460817febae
rabobahago <rabobahago@gmail.com>
2021-10-25 16:12:32 +0100

    refactor:App class component to functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-import React, { Component, Fragment } from "react";
+import React, { useEffect, Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { connect } from "react-redux";
+import { useDispatch } from "react-redux";
 import { handleInitialData } from "../actions/shared";
 import Header from "./Header";
 import PageNotFound from "./PageNotFound";
@@ -11,33 +11,32 @@ import LeaderBoard from "./LeaderBoard";
 import Question from "./Question";
 import Result from "./Result";
 
-class App extends Component {
-  componentDidMount() {
-    this.props.dispatch(handleInitialData());
-  }
-  render() {
-    return (
-      <div>
-        <Header />
-        <Router>
-          <Fragment>
-            <div className="container">
-              <Switch>
-                <Route path="/" exact component={Login} />
-                <Route path="/login/redirect/:id" exact component={Login} />
-                <Route path="/home" exact component={Home} />
-                <Route path="/questions/:id" exact component={Question} />
-                <Route path="/results/:id" exact component={Result} />
-                <Route path="/add" exact component={AddQuestion} />
-                <Route path="/leaderBoard" exact component={LeaderBoard} />
-                <Route component={PageNotFound} />
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
-      </div>
-    );
-  }
-}
-export default connect()(App);
-// export default App;
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(handleInitialData());
+  }, [dispatch]);
+  return (
+    <div>
+      <Header />
+      <Router>
+        <Fragment>
+          <div className="container">
+            <Switch>
+              <Route path="/" exact component={Login} />
+              <Route path="/login/redirect/:id" exact component={Login} />
+              <Route path="/home" exact component={Home} />
+              <Route path="/questions/:id" exact component={Question} />
+              <Route path="/results/:id" exact component={Result} />
+              <Route path="/add" exact component={AddQuestion} />
+              <Route path="/leaderBoard" exact component={LeaderBoard} />
+              <Route component={PageNotFound} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
